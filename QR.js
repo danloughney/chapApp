3c85b14ae2393b8831e4c5d14a3a858c94638725
danloughney <danloughney@gmail.com>
2019-12-18 08:43:02 -0500

    initial commit

--- QR.js
+++
@@ -1,8 +0,0 @@
-Google QR Code research
-
-Is this an authenticated API?
-
-https://foxlaneskiclub.wildapricot.org/dcl?ID=52486827
-
-https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=https://foxlaneskiclub.wildapricot.org/dcl?ID=52486827
-

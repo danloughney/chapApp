4f2c9d7624ea2023f800c0cdc48b1c08d5515680
danloughney <danloughney@gmail.com>
2020-02-11 21:40:48 -0500

    linting improvements

--- member.js
+++ member.js
@@ -4,19 +4,19 @@
 * Member ID page -- displays the member ID QR code
 */
 
-document.addEventListener("DOMContentLoaded", function(){         
-         var api = new WApublicApi('8jc86buyhc');
+document.addEventListener('DOMContentLoaded', function() {
+    const api = new WApublicApi('8jc86buyhc');
 
-         $.when(api.init()).done(function(){
-            api.apiRequest({
-               apiUrl: api.apiUrls.me(),
-               success: function (data, textStatus, jqXhr) {
-                    document.getElementById("qr").src = "https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=" + data.Id;
-                    if (document.getElementById("name")) {
-                        document.getElementById("name").innerHTML = data.FirstName + ' ' + data.LastName;
-                    } 
+    $.when(api.init()).done(function() {
+        api.apiRequest({
+            apiUrl: api.apiUrls.me(),
+            success: function(data, textStatus, jqXhr) {
+                document.getElementById('qr').src = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=' + data.Id;
+                if (document.getElementById('name')) {
+                    document.getElementById('name').innerHTML = data.FirstName + ' ' + data.LastName;
                 }
-           });
-           return false;
-       });
+            },
+        });
+        return false;
+    });
 });

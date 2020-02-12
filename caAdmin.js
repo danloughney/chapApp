1d26c4b91eb3927c654ed309a671c97865e623dc
danloughney <danloughney@gmail.com>
2020-02-11 21:39:59 -0500

    Audit report v1

--- caAdmin.js
+++ caAdmin.js
@@ -74,3 +74,104 @@ function changedLessonReport() {
     window.location.href='/caList?name=Lesson Changes';
 }
 
+function eventDescription(event) {
+    switch (event) {
+        case TripCheckInMorning:
+            return "Morning Check In";
+
+        case TripCheckInLunch:
+            return "Lunch Check In";
+
+        case TripCheckInLesson:
+            return "Lesson Check In";
+
+        case TripCheckInTesting:
+            return "Testing Check In";
+
+        case TripViolationDate:
+            return "Violations";
+
+        case TripTestDate:
+            return "Tested";
+
+        default:
+            return event;
+    }
+}
+
+
+class Audit {
+    constructor(name, event, ts, bus) {
+        this.name = name;
+        this.event = event;
+        this.ts = ts;
+        this.bus = bus || '';
+    }
+    html() {
+        return '<tr><td width="40%">%s</td><td width="25%">%s</td><td width="25%">%s</td><td width="25%">%s</td></tr>'.format(this.ts, this.event, this.name, this.bus);
+    }
+}
+// add firstaid link to the app (on top)
+
+const sortAudits = function(a, b) {
+    if (a == undefined && b != undefined) {
+        return -1;
+    } 
+    if (a != undefined && b == undefined) { 
+        return 1;
+    }
+    if (a == undefined && b == undefined) {
+        return 0;
+    }
+
+    var x = a.ts;//.toLowerCase();
+    var y = b.ts;//.toLowerCase();
+    return x < y ? -1 : x > y ? 1 : 0;
+};
+
+function tripAudit() {
+    document.getElementById('outputLegend').innerHTML = "Audit";
+    document.getElementById('outputText').innerHTML = 'Please wait...';
+
+    var events = [
+        TripCheckInMorning,
+        TripCheckInLunch,
+        TripCheckInLesson,
+        TripCheckInTesting,
+        TripViolationDate, 
+        //TripTestDate
+    ];
+
+    var auditLog = [];
+
+    $.api.apiRequest({
+        apiUrl: $.api.apiUrls.contacts({ '$filter' : "('Status' eq 'Active' AND 'TripCheckInMorning' ne NULL AND 'TripCheckInMorning' ne '')" }),
+        success: function (data, textStatus, jqXhr) {
+            document.getElementById('outputText').innerHTML = 'Got data...';
+
+            for (var i = 0; i < data.Contacts.length; i++) {
+                for (var j = 0; j < events.length; j++) {
+                    var ts = fieldValue(data.Contacts[i], events[j]);
+                    var bus = fieldValue(data.Contacts[i], TripBusNumber);
+                    if (ts != undefined && ts != '') {
+                        var audit = new Audit(data.Contacts[i].LastName + ', ' + data.Contacts[i].FirstName, events[j], ts, bus);
+                        auditLog.push(audit);
+                    }
+                }
+            }
+
+            auditLog = auditLog.sort(sortAudits);
+            var html = '<table width="100%">';
+            for (i = 0; i < auditLog.length; i++) {
+                html += auditLog[i].html();
+            }
+            html += '</table>';
+
+            document.getElementById('outputText').innerHTML = html;
+
+        },
+        error: function (data, textStatus, jqXhr) {
+            alert('audit query failed');
+        }
+    });
+}
